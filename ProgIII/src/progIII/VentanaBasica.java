62a0e3f6215e170a5d4811a7e7651e176b25b48f
Gaizka <Gaizka@LAPTOP-3IOH2VU2>
2019-09-24 08:39:00 +0200

    Creada estrucutura del proyecto

---
+++ ProgIII/src/progIII/VentanaBasica.java
@@ -0,0 +1,18 @@
+package progIII;
+
+import javax.swing.JFrame;
+
+public class VentanaBasica extends JFrame{
+	
+	public VentanaBasica(){
+		this.setSize(320, 240);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		 
+	}
+	
+public static void main(String[] args) {
+		
+	}
+
+
+}
